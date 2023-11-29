9f780aac4b9011248e15228ad6a8f89d811266e5
shahir <137508506+shahiirrrr@users.noreply.github.com>
2023-11-29 17:56:41 +0530

    Add files via upload

---
+++ flaasskkk/prg.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+
+
+app=Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
+@app.route('/index')
+def hello():
+    return render_template('index.html')
+
+
+if __name__ =='__main__':
+    app.run()
